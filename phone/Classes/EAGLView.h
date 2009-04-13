1aca41d14ab03602b9e041038c2f2b9a7100d1d6
Dave Peck <davepeck [at] davepeck [dot] org>
2009-04-12 23:28:04 -0700

    In which I learn some OpenGL (ES) and draw something in such a way that to do it right I actually have to understand the various matrices.

--- phone/Classes/EAGLView.h
+++ phone/Classes/EAGLView.h
@@ -17,29 +17,33 @@ This class wraps the CAEAGLLayer from CoreAnimation into a convenient UIView sub
 The view content is basically an EAGL surface you render your OpenGL scene into.
 Note that setting the view non-opaque will only work if the EAGL surface has an alpha channel.
 */
-@interface EAGLView : UIView {
-    
+@interface EAGLView : UIView
+{
 @private
-    /* The pixel dimensions of the backbuffer */
-    GLint backingWidth;
-    GLint backingHeight;
-    
-    EAGLContext *context;
-    
-    /* OpenGL names for the renderbuffer and framebuffers used to render to this view */
-    GLuint viewRenderbuffer, viewFramebuffer;
-    
-    /* OpenGL name for the depth buffer that is attached to viewFramebuffer, if it exists (0 if it does not exist) */
-    GLuint depthRenderbuffer;
-    
-    NSTimer *animationTimer;
-    NSTimeInterval animationInterval;
+	
+	/* The pixel dimensions of the backbuffer */
+	GLint backingWidth;
+	GLint backingHeight;
+	
+	EAGLContext *context;
+	
+	/* OpenGL names for the renderbuffer and framebuffers used to render to this view */
+	GLuint viewRenderbuffer, viewFramebuffer;
+	
+	/* OpenGL name for the depth buffer that is attached to viewFramebuffer, if it exists (0 if it does not exist) */
+	GLuint depthRenderbuffer;
+	
+	/* OpenGL name for the sprite texture */
+	GLuint spriteTexture;
+	
+	NSTimer *animationTimer;
+	NSTimeInterval animationInterval;
 }
-
-@property NSTimeInterval animationInterval;
 
 - (void)startAnimation;
 - (void)stopAnimation;
 - (void)drawView;
+
+@property NSTimeInterval animationInterval;
 
 @end
--- phone/Classes/EAGLView.m
+++ phone/Classes/EAGLView.m
@@ -13,187 +13,265 @@
 
 #import "EAGLView.h"
 
-#define USE_DEPTH_BUFFER 0
+@interface EAGLView (EAGLViewPrivate)
 
-// A class extension to declare private methods
-@interface EAGLView ()
-
-@property (nonatomic, retain) EAGLContext *context;
-@property (nonatomic, assign) NSTimer *animationTimer;
-
-- (BOOL) createFramebuffer;
-- (void) destroyFramebuffer;
+- (BOOL)createFramebuffer;
+- (void)destroyFramebuffer;
 
 @end
 
+@interface EAGLView (EAGLViewSprite)
+
+- (void)setupView;
+
+@end
 
 @implementation EAGLView
 
-@synthesize context;
-@synthesize animationTimer;
 @synthesize animationInterval;
 
-
-// You must implement this method
-+ (Class)layerClass {
-    return [CAEAGLLayer class];
+// You must implement this
++ (Class) layerClass
+{
+	return [CAEAGLLayer class];
 }
 
 
 //The GL view is stored in the nib file. When it's unarchived it's sent -initWithCoder:
-- (id)initWithCoder:(NSCoder*)coder {
-    
-    if ((self = [super initWithCoder:coder])) {
-        // Get the layer
-        CAEAGLLayer *eaglLayer = (CAEAGLLayer *)self.layer;
-        
-        eaglLayer.opaque = YES;
-        eaglLayer.drawableProperties = [NSDictionary dictionaryWithObjectsAndKeys:
-                                        [NSNumber numberWithBool:NO], kEAGLDrawablePropertyRetainedBacking, kEAGLColorFormatRGBA8, kEAGLDrawablePropertyColorFormat, nil];
-        
-        context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES1];
-        
-        if (!context || ![EAGLContext setCurrentContext:context]) {
-            [self release];
-            return nil;
-        }
-        
-        animationInterval = 1.0 / 60.0;
-    }
-    return self;
+- (id)initWithCoder:(NSCoder*)coder
+{
+	if((self = [super initWithCoder:coder])) {
+		// Get the layer
+		CAEAGLLayer *eaglLayer = (CAEAGLLayer*) self.layer;
+		
+		eaglLayer.opaque = YES;
+		eaglLayer.drawableProperties = [NSDictionary dictionaryWithObjectsAndKeys:
+										[NSNumber numberWithBool:FALSE], kEAGLDrawablePropertyRetainedBacking, kEAGLColorFormatRGBA8, kEAGLDrawablePropertyColorFormat, nil];
+		
+		context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES1];
+		
+		if(!context || ![EAGLContext setCurrentContext:context] || ![self createFramebuffer]) {
+			[self release];
+			return nil;
+		}
+		
+		animationInterval = 1.0 / 60.0;
+		
+		[self setupView];
+		[self drawView];
+	}
+	
+	return self;
 }
 
 
-- (void)drawView {
-    
-    // Replace the implementation of this method to do your own custom drawing
-    
-    const GLfloat squareVertices[] = {
-        -0.5f, -0.5f,
-        0.5f,  -0.5f,
-        -0.5f,  0.5f,
-        0.5f,   0.5f,
-    };
-    const GLubyte squareColors[] = {
-        255, 255,   0, 255,
-        0,   255, 255, 255,
-        0,     0,   0,   0,
-        255,   0, 255, 255,
-    };
-    
-    [EAGLContext setCurrentContext:context];
-    
-    glBindFramebufferOES(GL_FRAMEBUFFER_OES, viewFramebuffer);
-    glViewport(0, 0, backingWidth, backingHeight);
-    
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrthof(-1.0f, 1.0f, -1.5f, 1.5f, -1.0f, 1.0f);
-    glMatrixMode(GL_MODELVIEW);
-    glRotatef(3.0f, 0.0f, 0.0f, 1.0f);
-    
-    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    
-    glVertexPointer(2, GL_FLOAT, 0, squareVertices);
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glColorPointer(4, GL_UNSIGNED_BYTE, 0, squareColors);
-    glEnableClientState(GL_COLOR_ARRAY);
-    
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-    
-    glBindRenderbufferOES(GL_RENDERBUFFER_OES, viewRenderbuffer);
-    [context presentRenderbuffer:GL_RENDERBUFFER_OES];
+- (void)layoutSubviews
+{
+	[EAGLContext setCurrentContext:context];
+	[self destroyFramebuffer];
+	[self createFramebuffer];
+	[self drawView];
 }
 
 
-- (void)layoutSubviews {
-    [EAGLContext setCurrentContext:context];
-    [self destroyFramebuffer];
-    [self createFramebuffer];
-    [self drawView];
+- (BOOL)createFramebuffer
+{
+	glGenFramebuffersOES(1, &viewFramebuffer);
+	glGenRenderbuffersOES(1, &viewRenderbuffer);
+	
+	glBindFramebufferOES(GL_FRAMEBUFFER_OES, viewFramebuffer);
+	glBindRenderbufferOES(GL_RENDERBUFFER_OES, viewRenderbuffer);
+	[context renderbufferStorage:GL_RENDERBUFFER_OES fromDrawable:(id<EAGLDrawable>)self.layer];
+	glFramebufferRenderbufferOES(GL_FRAMEBUFFER_OES, GL_COLOR_ATTACHMENT0_OES, GL_RENDERBUFFER_OES, viewRenderbuffer);
+	
+	glGetRenderbufferParameterivOES(GL_RENDERBUFFER_OES, GL_RENDERBUFFER_WIDTH_OES, &backingWidth);
+	glGetRenderbufferParameterivOES(GL_RENDERBUFFER_OES, GL_RENDERBUFFER_HEIGHT_OES, &backingHeight);
+	
+	if(glCheckFramebufferStatusOES(GL_FRAMEBUFFER_OES) != GL_FRAMEBUFFER_COMPLETE_OES) {
+		NSLog(@"failed to make complete framebuffer object %x", glCheckFramebufferStatusOES(GL_FRAMEBUFFER_OES));
+		return NO;
+	}
+	
+	return YES;
 }
 
 
-- (BOOL)createFramebuffer {
-    
-    glGenFramebuffersOES(1, &viewFramebuffer);
-    glGenRenderbuffersOES(1, &viewRenderbuffer);
-    
-    glBindFramebufferOES(GL_FRAMEBUFFER_OES, viewFramebuffer);
-    glBindRenderbufferOES(GL_RENDERBUFFER_OES, viewRenderbuffer);
-    [context renderbufferStorage:GL_RENDERBUFFER_OES fromDrawable:(CAEAGLLayer*)self.layer];
-    glFramebufferRenderbufferOES(GL_FRAMEBUFFER_OES, GL_COLOR_ATTACHMENT0_OES, GL_RENDERBUFFER_OES, viewRenderbuffer);
-    
-    glGetRenderbufferParameterivOES(GL_RENDERBUFFER_OES, GL_RENDERBUFFER_WIDTH_OES, &backingWidth);
-    glGetRenderbufferParameterivOES(GL_RENDERBUFFER_OES, GL_RENDERBUFFER_HEIGHT_OES, &backingHeight);
-    
-    if (USE_DEPTH_BUFFER) {
-        glGenRenderbuffersOES(1, &depthRenderbuffer);
-        glBindRenderbufferOES(GL_RENDERBUFFER_OES, depthRenderbuffer);
-        glRenderbufferStorageOES(GL_RENDERBUFFER_OES, GL_DEPTH_COMPONENT16_OES, backingWidth, backingHeight);
-        glFramebufferRenderbufferOES(GL_FRAMEBUFFER_OES, GL_DEPTH_ATTACHMENT_OES, GL_RENDERBUFFER_OES, depthRenderbuffer);
-    }
-    
-    if(glCheckFramebufferStatusOES(GL_FRAMEBUFFER_OES) != GL_FRAMEBUFFER_COMPLETE_OES) {
-        NSLog(@"failed to make complete framebuffer object %x", glCheckFramebufferStatusOES(GL_FRAMEBUFFER_OES));
-        return NO;
-    }
-    
-    return YES;
+- (void)destroyFramebuffer
+{
+	glDeleteFramebuffersOES(1, &viewFramebuffer);
+	viewFramebuffer = 0;
+	glDeleteRenderbuffersOES(1, &viewRenderbuffer);
+	viewRenderbuffer = 0;
+	
+	if(depthRenderbuffer) {
+		glDeleteRenderbuffersOES(1, &depthRenderbuffer);
+		depthRenderbuffer = 0;
+	}
 }
 
 
-- (void)destroyFramebuffer {
-    
-    glDeleteFramebuffersOES(1, &viewFramebuffer);
-    viewFramebuffer = 0;
-    glDeleteRenderbuffersOES(1, &viewRenderbuffer);
-    viewRenderbuffer = 0;
-    
-    if(depthRenderbuffer) {
-        glDeleteRenderbuffersOES(1, &depthRenderbuffer);
-        depthRenderbuffer = 0;
-    }
+- (void)startAnimation
+{
+	animationTimer = [NSTimer scheduledTimerWithTimeInterval:animationInterval target:self selector:@selector(drawView) userInfo:nil repeats:YES];
 }
 
 
-- (void)startAnimation {
-    self.animationTimer = [NSTimer scheduledTimerWithTimeInterval:animationInterval target:self selector:@selector(drawView) userInfo:nil repeats:YES];
+- (void)stopAnimation
+{
+	[animationTimer invalidate];
+	animationTimer = nil;
 }
 
 
-- (void)stopAnimation {
-    self.animationTimer = nil;
+- (void)setAnimationInterval:(NSTimeInterval)interval
+{
+	animationInterval = interval;
+	
+	if(animationTimer) {
+		[self stopAnimation];
+		[self startAnimation];
+	}
 }
 
 
-- (void)setAnimationTimer:(NSTimer *)newTimer {
-    [animationTimer invalidate];
-    animationTimer = newTimer;
+// Sets up an array of values to use as the sprite vertices.
+const GLfloat spriteVertices[] = {
+0.0f, 0.0f,
+1.0f, 0.0f,
+0.0f, 1.0f,
+1.0f, 1.0f,
+
+/*
+-1.0f, -1.0f,
+1.0f, -1.0f,
+-1.0f, 1.0f,
+1.0f, 1.0f,
+*/
+
+/*
+-0.5f, -0.5f,
+0.5f, -0.5f,
+-0.5f,  0.5f,
+0.5f,  0.5f,
+ */
+};
+
+// Sets up an array of values for the texture coordinates.
+const GLshort spriteTexcoords[] = {
+0,1,
+1,1,
+0,0,
+1,0,
+/*
+0, 0,
+1, 0,
+0, 1, 
+1, 1,*/
+};
+
+- (void)setupView
+{
+	CGImageRef spriteImage;
+	CGContextRef spriteContext;
+	GLubyte *spriteData;
+	size_t	width, height;
+	
+	// Sets up matrices and transforms for OpenGL ES
+	glViewport(0, 0, backingWidth, backingHeight);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//glOrthof(-1.0f, 1.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+	glOrthof(0.0f, 1.0f, 0.0f, 1.5f, -1.0f, 1.0f);
+	
+	glMatrixMode(GL_MODELVIEW);
+	
+	
+	// DAVEPECK where does this belong in, really?
+	glTranslatef(0.0f, 0.25f, 0.0f);
+	
+	
+	// Clears the view with black
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	
+	// Sets up pointers and enables states needed for using vertex arrays and textures
+	glVertexPointer(2, GL_FLOAT, 0, spriteVertices);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glTexCoordPointer(2, GL_SHORT, 0, spriteTexcoords);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	
+	// Creates a Core Graphics image from an image file
+	spriteImage = [UIImage imageNamed:@"example-world-tiles-24-512.png"].CGImage;
+	// Get the width and height of the image
+	width = CGImageGetWidth(spriteImage);
+	height = CGImageGetHeight(spriteImage);
+	// Texture dimensions must be a power of 2. If you write an application that allows users to supply an image,
+	// you'll want to add code that checks the dimensions and takes appropriate action if they are not a power of 2.
+	
+	if(spriteImage) {
+		// Allocated memory needed for the bitmap context
+		spriteData = (GLubyte *) malloc(width * height * 4);
+		// Uses the bitmatp creation function provided by the Core Graphics framework. 
+		spriteContext = CGBitmapContextCreate(spriteData, width, height, 8, width * 4, CGImageGetColorSpace(spriteImage), kCGImageAlphaPremultipliedLast);
+		// After you create the context, you can draw the sprite image to the context.
+		CGContextDrawImage(spriteContext, CGRectMake(0.0, 0.0, (CGFloat)width, (CGFloat)height), spriteImage);
+		// You don't need the context at this point, so you need to release it to avoid memory leaks.
+		CGContextRelease(spriteContext);
+		
+		// Use OpenGL ES to generate a name for the texture.
+		glGenTextures(1, &spriteTexture);
+		// Bind the texture name. 
+		glBindTexture(GL_TEXTURE_2D, spriteTexture);
+		// Speidfy a 2D texture image, provideing the a pointer to the image data in memory
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, spriteData);
+		// Release the image data
+		free(spriteData);
+		
+		// Set the texture parameters to use a minifying filter and a linear filer (weighted average)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		
+		// Enable use of the texture
+		glEnable(GL_TEXTURE_2D);
+		// Set a blending function to use
+		glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+		// Enable blending
+		glEnable(GL_BLEND);
+	}
 }
 
-
-- (void)setAnimationInterval:(NSTimeInterval)interval {
-    
-    animationInterval = interval;
-    if (animationTimer) {
-        [self stopAnimation];
-        [self startAnimation];
-    }
+// Updates the OpenGL view when the timer fires
+- (void)drawView
+{
+	// Make sure that you are drawing to the current context
+	[EAGLContext setCurrentContext:context];
+	
+	glBindFramebufferOES(GL_FRAMEBUFFER_OES, viewFramebuffer);
+	// glRotatef(0.5f, 0.0f, 0.0f, 1.0f);
+	
+	// put the center point at 0,0
+	glTranslatef(0.5f, 0.5f, 0.0f);		/* make sure i understand these numbers */
+	glScalef(1.001f, 1.001f, 1.001f);
+	glTranslatef(-0.5f, -0.5f, 0.0f);	/* and these (do we lose centering here?) */	
+	
+	glClear(GL_COLOR_BUFFER_BIT);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	
+	glBindRenderbufferOES(GL_RENDERBUFFER_OES, viewRenderbuffer);
+	[context presentRenderbuffer:GL_RENDERBUFFER_OES];
 }
 
-
-- (void)dealloc {
-    
-    [self stopAnimation];
-    
-    if ([EAGLContext currentContext] == context) {
-        [EAGLContext setCurrentContext:nil];
-    }
-    
-    [context release];  
-    [super dealloc];
+// Stop animating and release resources when they are no longer needed.
+- (void)dealloc
+{
+	[self stopAnimation];
+	
+	if([EAGLContext currentContext] == context) {
+		[EAGLContext setCurrentContext:nil];
+	}
+	
+	[context release];
+	context = nil;
+	
+	[super dealloc];
 }
 
 @end
