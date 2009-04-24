84037894d1f62b040a9976e1229a40897b02b977
Dave Peck <davepeck [at] davepeck [dot] org>
2009-04-23 20:01:01 -0700

    Final flick dynamics code & comments.

--- phone/FlickDynamics.m
+++ phone/FlickDynamics.m
@@ -1,10 +1,38 @@
 //
 //  FlickDynamics.m
-//  iMurphy
+//  (c) 2009 Dave Peck <davepeck [at] davepeck [dot] org> 
+//  http://davepeck.org/
 //
-//  Created by Dave Peck <davepeck [at] davepeck [dot] org> on 4/23/09.
-//  Copyright 2009 Code Orange. All rights reserved.
+//  This code is released under the BSD license. If you use my code in your product,
+//  please put my name somewhere in the credits and let me know about it!
 //
+//  This code mimics the scroll/flick dynamics of the iPhone UIScrollView.
+//  What's cool about this code is that it is entirely independent of any iPhone
+//  UI, so you can use it to provide scroll/flick behavior on your custom views.
+//
+//  The key thing (which you'll learn fast if you try and build this yourself) is that
+//  you can't just rely on the last two points to compute your motion vector. Instead
+//  you need to "look back in time" to figure out where the touch was, say, 0.07 seconds
+//  ago. That will give you a much better sense of your vector and speed.
+//
+//  In order to answer the question "where was the touch 0.07 seconds ago" we keep a 
+//  history of the last N touches. When the user's touch is released, we look back through
+//  the history and use linear interpolation to determine where the touch _would have been_
+//  had we recorded a touch at exactly 0.07 seconds ago. We use that point as the basis
+//  for our motion vector. To ensure that we never scroll "too fast," we clamp down on
+//  any large motion that we compute, being sure to maintain the direction while reducing
+//  the magnitude of motion.
+//
+//  This code is coordinate system agnostic. I've chosen constants that made sense for 
+//  a coordinate system where the viewport is 1.0 by 1.0 in size. However, when you
+//  initialize this code, it will scale the constants as appropriate for your viewport size.
+//  (For example, it works fine if your viewport is 320 x 480 in size.)
+//
+//  This code expects that you already have an animation loop running. By default, the
+//  expectation is that you will call animate: sixty times a second. If you want to
+//  run at a different rate, be sure to initialize this class with your expected animation
+//  rate. Again, the built-in constants will be scaled to match.
+// 
 
 #import "FlickDynamics.h"
 
@@ -74,7 +102,7 @@ const NSUInteger DEFAULT_CAPACITY = 20;
 		double viewportAdjustment = (xAdjustment + yAdjustment) / 2.0;
 		
 		motionDamp = pow(DEFAULT_MOTION_DAMP, animationRateAdjustment);
-		motionMultiplier = DEFAULT_MOTION_MULTIPLIER * viewportAdjustment;
+		motionMultiplier = DEFAULT_MOTION_MULTIPLIER; /* does not need to be affected by viewportAdjustment */
 		motionMinimum = DEFAULT_MOTION_MINIMUM * viewportAdjustment;		
 		flickThresholdX = DEFAULT_FLICK_THRESHOLD * xAdjustment;
 		flickThresholdY = DEFAULT_FLICK_THRESHOLD * yAdjustment;
@@ -252,7 +280,7 @@ const NSUInteger DEFAULT_CAPACITY = 20;
 	last.y = y;
 	last.time = [[NSDate date] timeIntervalSince1970];
 	[self addToHistory:last];
-
+	
 	// do the standard scrolling motion in response
 	currentScrollLeft += (old.x - last.x);
 	currentScrollTop += (old.y - last.y);	
